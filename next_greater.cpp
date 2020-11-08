5187b97ccd15d6549881847709f25b99e394d094
Sumit Kumar <messikumar66@gmail.com>
2020-11-08 19:57:01 +0530

    Create next_greater.cpp

---
+++ next_greater.cpp
@@ -0,0 +1,12 @@
+int nextgreater[n];
+	for(int i = 0; i < n; ++i){
+		while(!stk.empty() && a[stk.top()] < a[i]){
+			nextgreater[stk.top()] = i;
+			stk.pop();
+		}
+		stk.push(i);
+	}
+	while(!stk.empty()){
+		nextgreater[stk.top()] = -1;
+		stk.pop();
+	}
