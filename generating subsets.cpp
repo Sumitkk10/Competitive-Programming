3ac92fff872779fcd5be5aceafec4c3843bf9646
Sumit Kumar <messikumar66@gmail.com>
2020-11-18 21:22:44 +0530

    Create generating subsets.cpp

---
+++ generating subsets.cpp
@@ -0,0 +1,11 @@
+// iterative;
+
+for (int b = 0; b < (1<<n); b++) {
+  vector<int> subset;
+  for (int i = 0; i < n; i++) {
+    if (b&(1<<i)) 
+      subset.push_back(i);
+  }
+  ans[cnt] = subset;
+  ++cnt;
+}
