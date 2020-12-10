2be1a5b645ee71459cb7e8cb3da776f25a165734
Sumit Kumar <messikumar66@gmail.com>
2020-12-10 14:04:07 +0530

    Update Longest Path in a DAG.cpp

--- Graphs/Longest Path in a DAG.cpp
+++ Graphs/Longest Path in a DAG.cpp
@@ -1,69 +1,66 @@
-// Minimum no of semesters needed to complete education..
+// Longest path in a DAG
 
 #include <bits/stdc++.h>
 #define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
 #define ll long long int
 #define ld long double
 using namespace std;
-const int N = 12;
+const int N = 1e6 + 5;
 const int MOD = 1e9 + 7;
-ll n, m, indegree[N], LPD[N];
-vector<ll> graph[N];
-bool vis[N];
+vector<int> graph[N];
 queue<int> q;
+int LPD[N], indegree[N];
+bool vis[N];
 
-void indegre(int source){
-    vis[source] = true;
-    for(auto i : graph[source]){
-        indegree[i]++;
-        if(!vis[i])
-            indegre(i);
-    }
+void dfs(int source){
+	vis[source] = 1;
+	for(auto kk : graph[source]){
+		indegree[kk]++;
+		if(!vis[kk])
+			dfs(kk);
+	}
 }
 
-void DAG(){
-    while(!q.empty()){
-        long long int node = q.front();
-        q.pop();
-        for(auto edge : graph[node]){
-            if(!vis[edge]){
-                indegree[edge]--;
-                if(indegree[edge] == 0){
-                    q.push(edge);
-                    vis[edge] = true;
-                }
-                LPD[edge] = max(LPD[(ll) edge], LPD[node] + 1);
-            }
-        }
-    }
+void topo(){
+	while(!q.empty()){
+		int x = q.front();
+		q.pop();
+		for(auto k : graph[x]){
+			if(!vis[k]){
+				indegree[k]--;
+				if(indegree[k] == 0){
+					q.push(k);
+					vis[k] = true;
+				}
+				LPD[k] = max(LPD[k], LPD[x] + 1);
+			}
+		}
+	}
 }
 
 int main(){
-    fast;
-    cin >> n >> m;
-    for(int i = 1; i <= m; ++i){
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-    }
-    for(int i = 1; i <= n; ++i){
-        if(!vis[i])
-            indegre(i);
-    }
-    memset(vis, 0, sizeof(vis));
-    for(int i = 1; i <= n; ++i){
-    	// cout << indegree[i] << " ";
-        if(indegree[i] == 0){
-            q.push(i);
-            vis[i] = true;
-        }
-    }
-    DAG();
-   	map<long long int, long long int> mp;
-   	for(long long int i = 1; i <= n; ++i){
-   		mp[LPD[i]]++;
-   		// cout << LPD[i] << " ";
-   	}
-   	cout << mp.size() << "\n";
-    return 0;
+	fast;
+	int n, m;
+	cin >> n >> m;
+	for(int i = 0; i < m; ++i){
+		int u, v;
+		cin >> u >> v;
+		graph[u].push_back(v);
+	}
+	for(int i = 1; i <= n; ++i)
+		if(!vis[i])
+			dfs(i);
+	memset(vis, 0, sizeof(vis));
+	for(int i = 1; i <= n; ++i){
+		if(!indegree[i]){
+			q.push(i);
+			vis[i] = true;
+		}
+	}
+	topo();
+	int ans = 0;
+	for(int i = 1; i <= n; ++i)
+		ans = max(ans, LPD[i]);
+	cout << ans << '\n';
+	return 0;
 }
