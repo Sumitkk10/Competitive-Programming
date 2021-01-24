589071136400109ec47479477931d41994418ea2
Sumit Kumar <messikumar66@gmail.com>
2021-01-24 07:24:53 +0530

    Update No of distinct elements in the range(seg_tree).cpp

--- Seg_Tree/No of distinct elements in the range(seg_tree).cpp
+++ Seg_Tree/No of distinct elements in the range(seg_tree).cpp
@@ -1,61 +1,96 @@
 #include <bits/stdc++.h>
 #define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
-#define ll long long int
-#define ld long double
 using namespace std;
 const int N = 1e6 + 5;
 const int MOD = 1e9 + 7;
-ll a[N];
-set<ll>* seg_tree;
+int n, a[N], next_occur[N];
+vector<int> seg_tree[4 * N];
+
+vector<int> merge(vector<int> l, vector<int> r){
+    vector<int> ans;
+    int i = 0, j = 0;
+    while(i < l.size() and j < r.size()){
+        if(l[i] <= r[j]) {
+            ans.push_back(l[i]);
+            ++i;
+        }
+        else{
+            ans.push_back(r[j]);
+            ++j;
+        }
+    }
+    while(i < l.size()){
+        ans.push_back(l[i]);
+        ++i;
+    }
+    while(j < r.size()){
+        ans.push_back(r[j]);
+        ++j;
+    }
+    return ans;
+}
 
 void build(int vertex, int l, int r){
-	if(l == r)
-		seg_tree[vertex].insert(a[l]);
-	else{
-		int mid = (l + r) / 2;
-		build(2 * vertex, l, mid);
-		build((2 * vertex) + 1, mid + 1, r);
-		seg_tree[vertex].insert(seg_tree[2 * vertex].begin(), seg_tree[2 * vertex].end());
-		seg_tree[vertex].insert(seg_tree[(2 * vertex) + 1].begin(), seg_tree[(2 * vertex) + 1].end());
-	}
+    if(r < l)
+        return;
+    if(l == r) {
+        seg_tree[vertex].push_back(next_occur[l]);
+        return;
+    }
+    int mid = (l + r) / 2;
+    build(2 * vertex, l, mid);
+    build(2 * vertex + 1, mid + 1, r);
+    seg_tree[vertex] = merge(seg_tree[2 * vertex], seg_tree[2 * vertex + 1]);
 }
 
-set<ll> distinct_query(int vertex, int l, int r, int x, int y){
-	set<ll> realans;
-	if(r < x || l > y || l > r) 
-		return realans;
-	if(x <= l && r <= y)
-		return seg_tree[vertex];
-	int mid = (l + r) / 2;
-	set<ll> ans = distinct_query(2 * vertex, l, mid, x, y);
-	realans.insert(ans.begin(), ans.end());
-	set<ll> ans1 = distinct_query((2 * vertex) + 1, mid + 1, r, x, y);
-	realans.insert(ans1.begin(), ans1.end());
-	return realans;
+long long int find(int vertex, int l, int r, int str, int end, int search){
+    if(l > end or r < str)
+        return 0;
+    if(l >= str and r <= end){
+        int low = 0, high = seg_tree[vertex].size(), ok = -1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(seg_tree[vertex][mid] > search){
+                ok = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        if(ok == -1)
+            return 0;
+        return seg_tree[vertex].size() - ok;
+    }
+    int mid = (l + r) / 2;
+    long long int ans = find(2 * vertex, l, mid, str, end, search);
+    ans += find(2 * vertex + 1, mid + 1, r, str, end, search);
+    return ans;
 }
 
-void init(ll n){
-	ll h = (int) ceil(log2(n));
-	h = (2 * pow(2, h)) - 1;
-	seg_tree = new set<ll>[h];
-}
-
-int main(){
-	fast;	
-	int n;
-	cin >> n;
-	for(int i = 0; i < n; ++i)
-		cin >> a[i];
-	init(n);
-	build(1, 0, n - 1);
-	int q;
-	cin >> q;
-	while(q--){
-		int x, y;
-		cin >> x >> y;
-		--x, --y;
-		set<ll> ans = distinct_query(1, 0, n - 1, x, y);
-		cout << ans.size() << '\n';
-	}
-	return 0;
+int main() {
+    fast;
+    cin >> n;
+    for(int i = 0; i < n; ++i)
+        cin >> a[i];
+    map<int, int> mp;
+    for(int i = 0; i < n; ++i){
+        if(mp[a[i]])
+            next_occur[mp[a[i]]] = i + 1;
+        mp[a[i]] = i + 1;
+    }
+    for(int i = 1; i <= n; ++i) {
+        if (!next_occur[i])
+            next_occur[i] = n + 1;
+        // cout << next_occur[i] << ' ';
+    }
+    // cout << endl;
+    build(1, 1, n);
+    int q;
+    cin >> q;
+    while(q--){
+        int l, r;
+        cin >> l >> r;
+        cout << find(1, 1, n, l, r, r) << '\n';
+    }
+    return 0;
 }
