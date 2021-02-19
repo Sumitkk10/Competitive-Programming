5c5f21218380b9fff5cb5e4c98172e2f1ea4f3b9
Sumit Kumar <messikumar66@gmail.com>
2021-02-19 14:59:23 +0530

    Update In-Out Dp on Trees.cpp

--- Graphs/In-Out Dp on Trees.cpp
+++ Graphs/In-Out Dp on Trees.cpp
@@ -1,3 +1,5 @@
+// calculate the node farthest from every node in a tree
+
 #include<bits/stdc++.h>
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
