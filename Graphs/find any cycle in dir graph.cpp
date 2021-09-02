968d12cefb45b94a33c8ce5d81352feddc043dcd
Sumit Kumar <messikumar66@gmail.com>
2021-09-02 10:15:14 +0530

    Create find any cycle in dir graph.cpp

---
+++ Graphs/find any cycle in dir graph.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+int n, m, par[N], str, finish, col[N];
+vector<int> graph[N];
+bool ok;
+
+void dfs(int node){
+    col[node] = 1;
+    for(auto k : graph[node]){
+        if(col[k] == 0){
+            par[k] = node;
+            dfs(k);
+        }
+        else if(col[k] == 1){
+            str = k;
+            finish = node;
+            ok = 1;
+        }
+    }
+    col[node] = 2;
+}
+
+void solve(){
+    cin >> n >> m;
+    for(int i = 1; i <= m; ++i){
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+    }
+    for(int i = 1; i <= n; ++i){
+        if(col[i]) continue;
+        dfs(i);
+        if(ok){
+            vector<int> ans;
+            ans.push_back(str);
+            while(finish != str){
+                ans.push_back(finish);
+                finish = par[finish];
+            }
+            ans.push_back(finish);
+            reverse(ans.begin(), ans.end());
+            cout << ans.size() << '\n';
+            for(auto k : ans) cout << k << ' ';
+            return;
+        }
+    }
+    cout << "IMPOSSIBLE\n";
+}
+
+int main(){
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--)
+        solve();
+    return 0;
+    // you should actually read the stuff at the bottom
+}
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?)
+    * do smth instead of nothing and stay organized
+    * WRITE STUFF DOWN
+    * DON'T GET STUCK ON ONE APPROACH
+    * Read other problems if stuck on this one.
+*/
